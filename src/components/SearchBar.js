8f4d9d7284b0289e4a409ce0ceb1246f38a73aa0
Onur Çankaya <onur.eren.cankaya@gmail.com>
2017-04-22 13:37:48 +0200

    initialized SearchBar component's state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        <p>Value of the input: {this.state.term}</p>
+      </div>
+    )
   }
 }
 
